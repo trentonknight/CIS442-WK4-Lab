3e84b8a80307f3e55585cf6e66fff7d045cb31e9
trentonknight <trentonknight@gmail.com>
2012-11-17 23:15:21 -0500

    looping through DEPT easy...

---
+++ beta.sql
@@ -0,0 +1,20 @@
+/*main branch to beta file for procedure*/
+CREATE OR REPLACE PROCEDURE newMESSAGES 
+IS
+BEGIN
+  DELETE FROM MESSAGES;
+  DECLARE
+  v_DepID varchar2(150);
+  v_Major varchar2(150);
+CURSOR c1 IS SELECT DEPTID FROM DEPT;
+  BEGIN
+    OPEN c1; 
+    LOOP
+      FETCH c1 INTO v_DepID;
+      EXIT WHEN c1%NOTFOUND;
+      DBMS_OUTPUT.PUT_LINE(RPAD(v_DepID, 7, ' '));
+    END LOOP;   
+    CLOSE c1; 
+  END newMESSAGE;
+END;      
+/                 
