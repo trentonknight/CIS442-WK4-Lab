c02dd821e1e9408687fc7a5d0a7044406870dc9d
trentonknight <trentonknight@gmail.com>
2012-11-18 12:38:52 -0500

    clean up...

---
+++ Mansfield-CIS442-Lab4.sql
@@ -0,0 +1,37 @@
+/*Jason N Mansfield*/
+/*CIS442 WK4*/
+/*Lab on Cursor Usage in Stored Procedures*/
+CREATE OR REPLACE PROCEDURE newMESSAGES 
+IS
+BEGIN
+/*clear out old table data*/
+DELETE FROM MESSAGES;
+DECLARE
+v_DepID varchar2(150);
+v_Major varchar2(150);
+CURSOR c1 IS SELECT DEPTID FROM DEPT;
+/*set cursor to select only matching majors with deptid*/
+CURSOR c2 IS SELECT DESCRIPTION FROM MAJOR WHERE DEPTID = v_DepID;
+BEGIN
+OPEN c1; 
+LOOP
+/*c1 loop*/
+FETCH c1 INTO v_DepID;
+EXIT WHEN c1%NOTFOUND;
+OPEN c2;
+LOOP
+/*nested c2 loop*/
+FETCH c2 INTO v_Major;
+EXIT WHEN c2%NOTFOUND;
+/*append matched depid to major*/
+INSERT INTO MESSAGES VALUES(v_DepID ||'*' || v_Major);
+/*end loop for cursor 2*/
+END LOOP;
+CLOSE c2;
+/*end loop for cursor 1*/
+END LOOP;
+CLOSE c1;
+/*end procedure*/
+END newMESSAGE;
+END;      
+/                 
