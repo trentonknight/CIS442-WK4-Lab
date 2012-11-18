c3c57bc6b819aa31508e2a2935127ff4200019ce
trentonknight <trentonknight@gmail.com>
2012-11-17 22:20:50 -0500

    procedure and beta cursor functional.

--- main.sql
+++ main.sql
@@ -1,13 +1,18 @@
-/*Table creation file: main.sql*/
 CREATE OR REPLACE PROCEDURE newMESSAGES 
-AUTHID CURRENT_USER
 IS
 BEGIN
-EXECUTE IMMEDIATE 
-CREATE TABLE MESSAGES
-(
-Results varchar2(100) NOT NULL
-);
-END
+DECLARE
+v_DepID varchar2(150);
+CURSOR c1 IS SELECT DEPTID FROM DEPT WHERE REGEXP_LIKE (DNAME, 'Computer Science');
+BEGIN
+OPEN c1; 
+LOOP
+FETCH c1 INTO v_DepID;
+EXIT WHEN c1%NOTFOUND;
+DBMS_OUTPUT.PUT_LINE(RPAD(v_DepID, 7, ' '));
+DBMS_OUTPUT.PUT_LINE( '-------------------------------------' );
+END LOOP;
+CLOSE c1; 
+END newMESSAGE;
+END;
 /
-/*end table creation*/
